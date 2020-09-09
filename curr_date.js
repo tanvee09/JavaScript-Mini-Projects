9fe94cdfb4b45f90787c57d0863f700aa60646e4
Tanvee Balhara <balharatanvee@gmail.com>
2020-09-09 21:01:10 +0530

    print current date

--- curr_date.js
+++ curr_date.js
@@ -1,7 +1,7 @@
 var today = new Date();
 
 var dd = today.getDate();
-var mm = today.getMonth() + 1; // No. getMonth() gives number of months elapsed till then
+var mm = today.getMonth() + 1; // getMonth() gives number of months elapsed till then
 var yy = today.getFullYear();
 
 if (dd < 10) {
