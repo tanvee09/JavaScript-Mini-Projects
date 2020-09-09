b9c4bd843b6f22accb4ddcaaa71c7215421e407c
Tanvee Balhara <balharatanvee@gmail.com>
2020-09-09 21:38:19 +0530

    Print current date, day, time

--- curr_date.js
+++ curr_date.js
@@ -1,5 +1,38 @@
 var today = new Date();
 
+var daynum = today.getDay();
+var day;
+
+switch(daynum) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+}
+
+var hr = today.getHours();
+var min = today.getMinutes();
+var sec = today.getSeconds();
+
+var timeNow = hr + ":" + min + ":" + sec;
+
 var dd = today.getDate();
 var mm = today.getMonth() + 1; // getMonth() gives number of months elapsed till then
 var yy = today.getFullYear();
@@ -13,4 +46,7 @@ if (mm < 10) {
 }
 
 today = dd + '/' + mm + '/' + yy;
-console.log("Today's date is: " + today);
+
+console.log("Date:  " + today);
+console.log("Day:   " + day);
+console.log("Time:  " + timeNow);
