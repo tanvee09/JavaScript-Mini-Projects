9b67bf60eff8fbc007744e7221817e915ce9156e
Tanvee Balhara <balharatanvee@gmail.com>
2020-09-09 21:00:39 +0530

    print current date

---
+++ curr_date.js
@@ -0,0 +1,16 @@
+var today = new Date();
+
+var dd = today.getDate();
+var mm = today.getMonth() + 1; // No. getMonth() gives number of months elapsed till then
+var yy = today.getFullYear();
+
+if (dd < 10) {
+    dd = '0' + dd;
+}
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+
+today = dd + '/' + mm + '/' + yy;
+console.log("Today's date is: " + today);
